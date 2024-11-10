bb34154933c77d29aaff0f2bb05a39110ab55173
Krystof2so <christo.2so@gmail.com>
2024-11-10 18:26:22 +0100

    (feat) fzf-lua
    Select 'fzf-native' as default profile

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -7,4 +7,4 @@ local fzf = require("fzf-lua")
 -- Enregistre fzf-lua comme interface par défaut pour vim.ui.select
 require('fzf-lua').register_ui_select()
 
-fzf.setup({})
+fzf.setup({'fzf-native'})  -- Choix du profil 'fzf-native'
