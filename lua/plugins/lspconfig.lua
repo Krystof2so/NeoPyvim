0b2e1c28bd230bd29cfe97eb741736923ec3acad
Krystof2so <christo.2so@gmail.com>
2025-06-11 23:11:10 +0200

    [Feat] linter
    add ruff

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,16 +2,36 @@
 -- * Configuration de ~/.config/nvim/lua/plugins/lspconfig.lua   *
 -- ***************************************************************
 
+---@diagnostic disable: undefined-global
+
 local lspconfig = require("lspconfig")
 
 -- Configuration des serveurs LSP
-local servers = { 'pyright', 'lua_ls', 'texlab' }
+local servers = { 'pyright', 'lua_ls', 'texlab', 'ruff' }
 
 --mason_lspconfig.setup({
   --ensure_installed = servers, -- Pour que les serveurs LSP spécifiés dans la table soiennt installés par Mason.
 --})
 
 for _, server in ipairs(servers) do
-  lspconfig[server].setup({})  -- Configure chaque serveur LSP avec les options par défaut
+    if server == 'ruff_lsp' then
+        lspconfig.ruff_lsp.setup({
+            on_attach = function(client, bufnr)
+                -- Configuration de l'attachement du client LSP
+                local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+                local opts = { noremap = true, silent = true }
+
+                -- Keymaps pour Ruff
+                buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+                buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+                buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+            end,
+            settings = {
+                -- Configuration spécifique à Ruff
+            },
+        })
+    else
+        lspconfig[server].setup({})  -- Configure chaque serveur LSP avec les options par défaut
+    end
 end
 
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,8 +6,12 @@
 local mason = require("mason")
 
 mason.setup({
-    ensure_installed = { "lua-language-server", "treesitter-lua",
-                         "pyright", "texlab" }, -- Installe le parser Lua pour Tree-sitter
+    ensure_installed = { "lua-language-server",
+                         "treesitter-lua",  -- Installe le parser Lua pour Tree-sitter
+                         "pyright",
+                         "texlab",
+                         "ruff",
+    },
     max_concurrent_installers = 6, -- Autorise jusqu'à 6 installations en parallèle
     ui = { -- Paramètres liés à l'interface
         border = "rounded",  -- Type de bordure
